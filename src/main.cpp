0851e31c6682626dc694a042f6726dc94b1eca9c
Brumse <froderberg89@gmail.com>
2025-03-26 21:02:38 +0100

    Make compile with external sdk folder

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+
+const char* ssid = "SSID"; // Replace with your Wi-Fi SSID
+const char* password = "PASSWORD"; // Replace with your Wi-Fi password
+
+int main() {
+    stdio_init_all();
+
+    if (cyw43_arch_init_with_country(CYW43_COUNTRY_UK)) {
+        printf("failed to initialise\n");
+        return 1;
+    }
+    printf("initialised\n");
+
+    cyw43_arch_enable_sta_mode();
+
+    printf("Connecting to WiFi...\n");
+    if (cyw43_arch_wifi_connect_timeout_ms(ssid, password, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+        printf("failed to connect\n");
+        return 1;
+    } else {
+        printf("Connected to WiFi\n");
+    }
+
+    while (true) {
+        sleep_ms(1000);
+    }
+
+    return 0;
+}
