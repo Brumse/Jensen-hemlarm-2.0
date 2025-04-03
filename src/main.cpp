36b9cb45312ea46d0d20d184602120a7cb22f6da
Brumse <froderberg89@gmail.com>
2025-04-03 11:32:15 +0200

    added passwordheader,include the header in cmake and in main. update gitignore to not include the passwordheader.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
+#include "wifi_credentials.h"
 
 const char* ssid = "SSID"; // Replace with your Wi-Fi SSID
 const char* password = "PASSWORD"; // Replace with your Wi-Fi password
@@ -8,7 +9,7 @@ const char* password = "PASSWORD"; // Replace with your Wi-Fi password
 int main() {
     stdio_init_all();
 
-    if (cyw43_arch_init_with_country(CYW43_COUNTRY_UK)) {
+    if (cyw43_arch_init()) {
         printf("failed to initialise\n");
         return 1;
     }
@@ -17,9 +18,10 @@ int main() {
     cyw43_arch_enable_sta_mode();
 
     printf("Connecting to WiFi...\n");
-    if (cyw43_arch_wifi_connect_timeout_ms(ssid, password, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID.c_str(), WIFI_PASSWORD.c_str(), CYW43_AUTH_WPA2_AES_PSK, 30000)) {
         printf("failed to connect\n");
-        return 1;
+        cyw43_arch_deinit();
+	return 1;
     } else {
         printf("Connected to WiFi\n");
     }
