01a7d3432163e6dfd6cfa18e98f13d1aef7335a6
Brumse <froderberg89@gmail.com>
2025-04-17 15:16:43 +0200

    Moved sensor info to conf header and own cpp, edit cmake

--- include/sensor_config.h
+++ include/sensor_config.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <stdio.h>
 /*
 NOTES for adding functions:
--- src/mqttmain.cpp
+++ src/mqttmain.cpp
@@ -19,8 +19,8 @@
 #include "lcd_printer.h"
 #include "pico/binary_info.h"
 #include "mqtt_config.h"
+#include "sensor_functions.h"
 #include "sensor_config.h"
-
 // Custom struct definitions
 
 /**
@@ -43,33 +43,6 @@ static void mqtt_connection_cb(mqtt_client_t *client, void *arg,
 static void pub_request_cb(void *arg, err_t err);
 static void publish_worker_fn(async_context_t *context,
                               async_at_time_worker_t *worker);
-
-void trigger_pulse()
-{
-        gpio_put(TRIG_PIN, 0);
-        sleep_us(5);
-        gpio_put(TRIG_PIN, 1);
-        sleep_us(10);
-        gpio_put(TRIG_PIN,0);
-}
-
-float get_distance()
-{
-    trigger_pulse();
-
-    // Wait for echo pulse
-    while(gpio_get(ECHO_PIN) == 0) tight_loop_contents();
-    absolute_time_t start_time = get_absolute_time();
-
-    while(gpio_get(ECHO_PIN) == 1) tight_loop_contents();
-    absolute_time_t end_time = get_absolute_time();
-
-    // Calculate duration in microseconds
-    int64_t pulse_duration = absolute_time_diff_us(start_time, end_time);
-    return (pulse_duration * 0.0343) / 2;
-}
-
-
 
 
 // Global objects
