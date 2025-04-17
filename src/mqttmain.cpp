47aeb7d14b66ef820e146da6a7d12dc05d6dbd45
Brumse <froderberg89@gmail.com>
2025-04-17 21:41:48 +0200

    move struct to headerfile

--- src/mqttmain.cpp
+++ src/mqttmain.cpp
@@ -21,20 +21,8 @@
 #include "mqtt_config.h"
 #include "sensor_functions.h"
 #include "sensor_config.h"
-// Custom struct definitions
+#include "mqtt_client_data.h"
 
-/**
- * @brief All data related to mqtt connection
- */
-typedef struct {
-  mqtt_client_t *mqtt_client; ///< Native mqtt client object
-  struct mqtt_connect_client_info_t mqtt_client_info; ///< Native client info
-  ip_addr_t mqtt_server_address;                      ///< Broker address
-  int mqtt_server_port;                               ///< Broker port
-  char topic[MQTT_TOPIC_LEN];                         ///< Topic to publish to
-  bool connect_done;                                  ///< Status of connection
-  int published_messages; ///< Simple pay load used when publishing
-} mqtt_client_data_t;
 
 // Forward declarations
 static void start_client(mqtt_client_data_t *state);
